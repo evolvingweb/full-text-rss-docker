ff6e64a1ca5e0e2aeb6e9700b4504dea174d9fda
Dave Vasilevsky <dave@vasilevsky.ca>
2014-10-18 01:27:02 +0000

    Caching is stupid

--- custom_config.php
+++ custom_config.php
@@ -6,6 +6,4 @@ if (!isset($options)) $options = new stdClass();
 $options->max_entries = 1000;
 $options->default_entries = $options->max_entries;
 
-$options->caching = true;
-$options->cache_time = 60 * 24 * 30; // 30 days
-$options->cache_cleanup = 1000000; // clear cache after this many runs
+$options->caching = false;
